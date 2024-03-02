769092f5e123bd52d5f97d8e51d20fd31ce968eb
Kku05 <95527930+Kku05@users.noreply.github.com>
2024-03-02 06:50:20 +0000

    yes

---
+++ app.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import customtkinter as ctk
+import torch
+import numpy as np
+import cv2
+from PIL import Image, ImageTk
+import vlc
+
+App = tk.Tk()
+App.geometry("600x600")
+App.title("drowsy boi 1")
+ctk.set_appearance_mode("dark")
